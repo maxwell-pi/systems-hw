4e5f9323e74741386ad846ea0e968d3fc1751dcc
Maxwell Pickering <maxwell-pi@Maxwells-MacBook-Air.local>
2025-04-08 00:15:05 -0400

    Dockerfile and db path fix.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python=3.12-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+ENV FLASK_RUN_HOST=0.0.0.0
+ENV FLASK_APP = run.py
+
+CMD ["flask", "run"]
--- notebook/__init__.py
+++ notebook/__init__.py
@@ -1,10 +1,13 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '928dc7c3e8db2b0ea72c9a499d64ac28'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db_path = os.environ.get("DATABASE_PATH", 'instance/notes.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
